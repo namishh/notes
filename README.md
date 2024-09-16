5fb6b8aa14e3f15434fb78c88c4092623f313236
nam <namish@tuta.io>
2024-09-16 15:03:45 +0530

    hover diff

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ My handcrafted DSA and programming notes. Please do not waste your savings on "b
   - [x] [Memory Management For Dummies](https://notes.namishh.me/pre/memory)
 - Algorithms
   - [x] [Time Complexity](https://notes.namishh.me/algo/complexity)
-  - [ ] Recursion
+  - [x] [Recursion](https://notes.namishh.me/algo/recursion)
   - [ ] Bit Manipulation
 - Data Structures
   - [ ] Queues
@@ -27,7 +27,7 @@ My handcrafted DSA and programming notes. Please do not waste your savings on "b
     - [ ] Doubly
     - [ ] Circular
   - [ ] Tree
-    - [ ] Binary
+    - [x] [Binary](https://notes.namishh.me/struct/binary-tree)
     - [ ] BST
     - [ ] B-Trees
     - [ ] AVL Trees
---
+++ comp/entry.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Entry = ({ text, link, level }) => {
+  let t = "";
+  let color = "";
+  if (level == "1") {
+    t = "Novice";
+    color = "bg-primary";
+  } else if (level == "2") {
+    t = "Amateur";
+    color = "difficulty-amateur";
+  } else if (level == "3") {
+    t = "Intermediate";
+    color = "difficulty-intermediate";
+  } else if (level == "4") {
+    t = "Advance";
+    color = "difficulty-advance";
+  } else if (level == "5") {
+    t = "Expert";
+    color = "difficulty-expert";
+  }
+  return (
+    <div className="flex w-full justify-between group items-center relative entry">
+      <div className="marker w-32">
+        <div className={`py-2 progress progress-${level}`}></div>
+      </div>
+      <a href={link}>{text}</a>
+      <span className={`difficulty ${color}`}>{t}</span>
+    </div>
+  );
+};
+
+export default Entry;
